6e22b08434f39ebfb226e178aa7acdc9500ad2f7
horrorschau105 <piotrekgdowski@wp.pl>
2016-12-02 22:58:23 +0100

    some notes for tomorrow

--- main/main/Program.cs
+++ main/main/Program.cs
@@ -10,6 +10,15 @@ namespace main
     {
         static void Main(string[] args)
         {
+            /** what to do
+             * generate graph (at first directed, 5 paths from 1 verticle)
+             * set begin and end
+             * compute with dijskra best way
+             * let ants go
+             * wait for results
+             * 
+             * /
+
         }
     }
 }
