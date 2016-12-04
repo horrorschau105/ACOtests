6e751d315d4b0f6d33c005349926acd7c23db546
horrorschau105 <piotrekgdowski@wp.pl>
2016-12-04 22:28:25 +0100

    Choosing paths, some tests

--- main/main/Ant.cs
+++ main/main/Ant.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace main
 {
-    class Ant
+    public class Ant
     {
         public int position; // current verticle
+        public int lengthOfWay;
         public List<int> way; // list of visited verticles
         public bool searchingFood;
         public double pheromoneProduction; // how much is it producting pheromon
@@ -18,17 +19,44 @@ namespace main
             searchingFood = true;
             pheromoneProduction = phP;
             way = new List<int>();
+            lengthOfWay = 0;
         }
         public void LeavePheromone(Path path)
         {
             path.AddPheromone(pheromoneProduction);
         }
-        public void ChooseWay(List<Path> p)
+        public Path ChooseWay(List<Path> p)
         {
-            /* take all posible paths
-             * sum up all pheremone 
-             */
-        
+            // List<Path> possibly = new List<Path>();
+            double lower; // sum by all possible paths with pheromone 
+            lower = 0; // mianownik
+            foreach(Path path in p)
+            {
+                    lower += Math.Pow(path.pheromone, Program.PHERO_CONST) * 
+                             Math.Pow((1.0 / path.length), Program.LENGTH_CONST);
+            }
+            Random r = new Random();
+            double rand = r.NextDouble();
+            foreach(Path path in p)
+            {
+                rand -= (Math.Pow(path.pheromone, Program.PHERO_CONST) * 
+                         Math.Pow((1.0 / path.length), Program.LENGTH_CONST)) / lower;
+                if (rand < 0) return path;
+            }
+            return p.Last();
+         }
+        public int Start()
+        {
+            while (this.position != Program.VERTICLES)
+            {
+                Path next = this.ChooseWay(Program.graph[this.position]);
+                next.AddPheromone(this.pheromoneProduction);
+                this.position = next.to;
+                this.lengthOfWay += next.length;
+                Program.EvaporateAllPaths(Program.graph, next.length / Program.MAXLENGTH);
+
+            }
+            return this.lengthOfWay;
         }
     }
 }
--- main/main/Program.cs
+++ main/main/Program.cs
@@ -8,11 +8,12 @@ namespace main
 {
     partial class Program
     {
-        public const int N = 1000;// degree of Rudy's graph
+        public const int N = 10;// degree of Rudy's graph
         public const int VERTICLES = 8 * N + 5;
         public const int MAXLENGTH = 200;// max length of path
         public const double PHERO_CONST = 0.1;
         public const double LENGTH_CONST = 0.1;
+        public static List<List<Path>> graph = GenerateGraph(N);
 
         static void Main(string[] args)
         {
@@ -24,15 +25,31 @@ namespace main
              * wait for results
              * 
              */
-            List<List<Path>> graph = GenerateGraph(N);
-            foreach(List<Path> ver in graph)
+           
+            /*foreach(List<Path> ver in graph)
             {
                 foreach(Path p in ver)
                 {
-            //        Console.WriteLine("{0}\t{1}\t{2}", p.from, p.to, p.length);
+                   Console.WriteLine("{0}\t{1}\t{2}", p.from, p.to, p.length);
                 }
-            }
+            }*/
             Console.WriteLine("Shortest Path: {0}", CalcShortPath(graph, 0, VERTICLES));
+            Ant a;
+            for(int i=0;i<100;++i)
+            {
+                a = new Ant(0.5);
+                Console.WriteLine(a.Start());
+            }
+            /*while(a.position != VERTICLES)
+            {
+                Path next = a.ChooseWay(graph[a.position]);
+                next.AddPheromone(a.pheromoneProduction);
+                a.position = next.to;
+                a.lengthOfWay += next.length;
+                EvaporateAllPaths(graph, next.length / MAXLENGTH);
+
+            }*/
+            //Console.WriteLine(a.lengthOfWay);
             // ok, we have a graph
             /* now some ants
              * moving this way: repeat(move, evaporate)
