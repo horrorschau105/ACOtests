0fd4b2ccf10d11781aa4d550b4f95c8e3998866d
horrorschau105 <piotrekgdowski@wp.pl>
2016-12-15 00:11:17 +0100

    nix

--- main/main/Program.cs
+++ main/main/Program.cs
@@ -8,29 +8,51 @@ namespace main
 {
     partial class Program
     {
-        public const int Ants = 200; // count of ants;
-        public const int Iterations = 100000;
-        public const int Degree = 100;// degree of Rudy's graph
-        public const int Verticles = 8 * Degree + 5;
-        public const int MaxLength = 10;// max length of path
-        public const double Alpha = 1.9; // pheromone
-        public const double Beta = 8.45; // length
-        public const double Ro = 0.21; // evaporating
-        public const double Q = 2.14; // adding pheromone
-        public const double Bonus = 7.3; // prize for finding better way
-        public static List<List<Path>> graph = GenerateGraph(Degree, Verticles);
-
-        static void Main(string[] args)
+        static public int Ants = 20; // count of ants;
+        static public int Iterations = 1000;
+        static public int Degree = 50;// degree of Rudy's graph
+        static public int Verticles = 8 * Degree + 5;
+        static public int MaxLength = 5;// max length of path
+        static public double Alpha = 1; // pheromone
+        static public double Beta = 1; // length
+        static public double Ro = 0.3; // evaporating
+        static public double Q = 2.14; // adding pheromone
+        static public double Bonus = 7; // prize for finding better way
+        static void Main()
         {
+            double min = int.MaxValue;
+            double curr;
+            for (int i = 0; i < 1000; ++i)
+            {
+                Alpha += 0.01;
+                for (int j = 0; j < 1000; ++j)
+                {
+                    Beta += 0.01;
+                    curr = Testbild();
+                    if (min > curr)
+                    {
+                        min = curr;
+                        Console.WriteLine("{0}\t{1}\t{2}", Alpha, Beta, curr);
+                    }
+                }
+                Beta -= 10;
+            }
+            Console.Write(min);
+            Console.Read();
+        }
+        static double Testbild()
+        {
+
+            List<List<Path>> graph = GenerateGraph(Degree, Verticles);
             int best = CalcShortPath(graph, 0, Verticles);
             List<Ant> ants = new List<Ant>();
             List<int> results = new List<int>() { MaxLength * Verticles };
-            for (int j = 0; j < Ants; j++)  ants.Add(new Ant());
+            for (int j = 0; j < Ants; j++) ants.Add(new Ant());
             double n = 0.0;
             Random r = new Random(105);
-            for (int i = 0; i < Iterations; ++i) 
+            for (int i = 0; i < Iterations; ++i)
             {
-                foreach(var ant in ants)
+                foreach (var ant in ants)
                 {
                     if (ant.position == Verticles)  // in last verticle, move to start and update pheromones
                     {
@@ -38,13 +60,13 @@ namespace main
                         if (results.Min() > ant.lengthOfWay) ant.way.ForEach(path => path.pheromone += Bonus * delta);
                         else ant.way.ForEach(path => path.pheromone += delta);
                         results.Add(ant.lengthOfWay);
-                   //     Console.WriteLine("{0}#: {1}", results.Count, ant.lengthOfWay);
+                        //    Console.WriteLine("{0}#: {1}", results.Count, ant.lengthOfWay);
                         ant.Clear();
                         graph.ForEach(list => list.ForEach(path => path.pheromone *= (1 - Ro)));
                         graph.ForEach(list => list.ForEach(path => path.pheromone = (path.pheromone < 1.0 / Verticles ? 1.0 / Verticles : path.pheromone)));
                     }
                     n = graph[ant.position].Sum((path => path.GetMultiplier())); // :)
-                    foreach(var path in graph[ant.position])
+                    foreach (var path in graph[ant.position])
                     {
                         if (r.NextDouble() > path.GetMultiplier() / n) continue;
                         ant.Move(path);
@@ -52,14 +74,11 @@ namespace main
                     }
                 }
             }
-            Console.WriteLine($"Best found: {results.Min()}, Difference from best: {diff(results.Min(), best)}");
-            Console.WriteLine($"Shortest Path: {best}");
-            Console.WriteLine("fin");
-            Console.Read();
+            return diff(results.Min(), best);
         }
         static double diff(int res, int best)
         {
-            return Math.Abs(best - res)* 1.0 / res;
+            return Math.Abs(best - res) * 1.0 / res;
         }
     }
 }
