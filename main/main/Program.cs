7d8f8b9c0530ea959a540fac91c824fc4ea35bf3
horrorschau105 <piotrekgdowski@wp.pl>
2016-12-14 23:12:10 +0100

    Having fun with branches

--- main/main/Program.cs
+++ main/main/Program.cs
@@ -5,9 +5,9 @@ namespace main
 {
     partial class Program
     {
-        public const int Ants = 100; // count of ants;
-        public const int Iterations = 5000;
-        public const int Verticles = 10;
+        public const int Ants = 20; // count of ants;
+        public const int Iterations = 500;
+        public const int Verticles = 5;
         public const int MaxLength = 9;// max length of path
         public const double Alpha = 1; // wykladnik
         public const double Beta = 1; // wykladnik
@@ -19,7 +19,6 @@ namespace main
         static void Main(string[] args)
         {
             int best = ShortestCircuit();
-            Console.WriteLine("mrufki");
             List<Ant> ants = new List<Ant>();
             List<Path> bbb = new List<Path>();
             List<int> results = new List<int>() { MaxLength * Verticles};
@@ -35,7 +34,8 @@ namespace main
                     {
                         ant.lengthOfWay += graph[ant.start_city][ant.way.Last().to].Length;
                         ant.way.Add(graph[ant.way.Last().to][ant.start_city]);
-                        if (ant.lengthOfWay < results.Min()) bbb = ant.way;
+                        if (ant.lengthOfWay < results.Min())
+                        { bbb = ant.way;  Console.WriteLine(ant.lengthOfWay); }
                         results.Add(ant.lengthOfWay);
                         var delta = Q / ant.lengthOfWay;
                         ant.way.ForEach(path => path.pheromone += Bonus * delta);
@@ -57,7 +57,7 @@ namespace main
             Console.WriteLine($"Best global: {best}");
             Console.WriteLine($"Best found: {results.Min()}");
             foreach (var p in bbb) Console.Write(p.from);
-            Console.WriteLine("fin");
+            Console.WriteLine("\nfin");
             Console.Read();
         }
     }
